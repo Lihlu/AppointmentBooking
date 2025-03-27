3eaad59f7a2f16066180211612a652d8a337ea6d
Lihlumise Njani <lihlumisenjani@outlook.com>
2025-03-27 19:07:20 +0200

    Added properties for role and tokens

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -10,6 +10,10 @@ namespace Domain.Models.Entities
         public required string Surname { get; set; }
         public required string Email { get; set; }
         public required string PasswordHash { get; set; }
+
+        public required string Role { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
         public List<Appointment>? Appointments { get; set; }
     }
 }
